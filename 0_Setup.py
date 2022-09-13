52c3ff8522572c21dc63e7399ed53607280159c6
Paul de Fusco <pauldefusco@cloudera.com>
2022-09-13 22:46:14 +0000

    working on reproducing project

--- 0_Setup.py
+++ 0_Setup.py
@@ -2,43 +2,53 @@
 #Copyright (c) 2022 Cloudera, Inc. All rights reserved.
 #
 
-### Installing Requirements
 !pip3 install -r requirements.txt
 
+# Create the directories and upload data
+from cmlbootstrap import CMLBootstrap
+from IPython.display import Javascript, HTML
 import os
 import time
 import json
 import requests
 import xml.etree.ElementTree as ET
 import datetime
-import yaml
+import subprocess
 
-#Extracting the correct URL from hive-site.xml
-tree = ET.parse('/etc/hadoop/conf/hive-site.xml')
-root = tree.getroot()
+run_time_suffix = datetime.datetime.now()
+run_time_suffix = run_time_suffix.strftime("%d%m%Y%H%M%S")
 
-for prop in root.findall('property'):
-    if prop.find('name').text == "hive.metastore.warehouse.dir":
-        storage = prop.find('value').text.split("/")[0] + "//" + prop.find('value').text.split("/")[2]
+# Set the setup variables needed by CMLBootstrap
+HOST = os.getenv("CDSW_API_URL").split(":")[0] + "://" + os.getenv("CDSW_DOMAIN")
+USERNAME = os.getenv("CDSW_PROJECT_URL").split("/")[6]  # args.username  # "vdibia"
+API_KEY = os.getenv("CDSW_API_KEY")
+PROJECT_NAME = os.getenv("CDSW_PROJECT")
 
-print("The correct Cloud Storage URL is:{}".format(storage))
+# Instantiate API Wrapper
+cml = CMLBootstrap(HOST, USERNAME, API_KEY, PROJECT_NAME)
 
-os.environ['STORAGE'] = storage
+# Set the STORAGE environment variable
+try:
+    storage = os.environ["STORAGE"]
+except:
+    if os.path.exists("/etc/hadoop/conf/hive-site.xml"):
+        tree = ET.parse("/etc/hadoop/conf/hive-site.xml")
+        root = tree.getroot()
+        for prop in root.findall("property"):
+            if prop.find("name").text == "hive.metastore.warehouse.dir":
+                storage = (
+                    prop.find("value").text.split("/")[0]
+                    + "//"
+                    + prop.find("value").text.split("/")[2]
+                )
+    else:
+        storage = "/user/" + os.getenv("HADOOP_USER_NAME")
+    storage_environment_params = {"STORAGE": storage}
+    storage_environment = cml.create_environment_variable(storage_environment_params)
+    os.environ["STORAGE"] = storage
 
-### Downloading Lab Files
+### Loading Data to Cloud Storage
 
-!wget https://www.cloudera.com/content/dam/www/marketing/tutorials/cdp-using-cli-api-to-automate-access-to-cloudera-data-engineering/tutorial-files.zip
-!mv tutorial-files.zip /home/cdsw/data
-!unzip /home/cdsw/data/tutorial-files.zip -d /home/cdsw/data
-
-!hdfs dfs -mkdir -p $STORAGE/datalake/cde-demo
-!hdfs dfs -copyFromLocal /home/cdsw/data/PPP-Over-150k-ALL.csv $STORAGE/datalake/cde-demo/PPP-Over-150k-ALL.csv
-!hdfs dfs -copyFromLocal /home/cdsw/data/PPP-Sub-150k-TX.csv $STORAGE/datalake/cde-demo/PPP-Sub-150k-TX.csv
-!hdfs dfs -ls $STORAGE/datalake/cde-demo
-
-!hdfs dfs -copyFromLocal /home/cdsw/data/LoanStats_2015_subset_071821.csv $STORAGE/datalake/cde-demo/LoanStats_2015_subset_071821.csv
-
-!rm /home/cdsw/data/PPP-Over-150k-ALL.csv /home/cdsw/data/PPP-Sub-150k-TX.csv /home/cdsw/data/config.yaml
-!rm /home/cdsw/data/Data_Extraction_Over_150k.py /home/cdsw/data/Data_Extraction_Sub_150k.py
-!rm /home/cdsw/data/tutorial-files.zip
-!rm /home/cdsw/data/Create_Reports.py
+!hdfs dfs -mkdir -p $STORAGE/datalake/model_factory
+!hdfs dfs -copyFromLocal /home/cdsw/data/LoanStats_2015_subset_091322.csv $STORAGE/datalake/model_factory/LoanStats_2015_subset_091322.csv
+!hdfs dfs -ls $STORAGE/datalake/model_factory
